21d45d6e8f875390adde2152be2cdadd17977664
HWCho <hwcho1986@gmail.com>
2026-02-04 09:29:50 +0900

    deploy: 로딩 문제 해결 및 index.html 단일 파일 구성으로 배포

--- components/Sidebar.tsx
+++
@@ -1,3 +0,0 @@
-
-// Redundant component removed. Logic integrated into Header.tsx
-export default () => null;
--- index.tsx
+++
@@ -1,3 +0,0 @@
-
-// 이 파일의 내용은 index.html 내부의 스크립트 태그로 통합되었습니다.
-// GitHub Pages 호환성을 위해 index.html을 직접 실행하세요.
